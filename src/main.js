7e8ae1da518190b093b64d8db3b200967f8113a9
amyjzhu <a.zhu@alumni.ubc.ca>
2017-07-12 16:32:00 -0700

    Refactored system for testability; XHRHttp requests used instead of http from NodeJS

--- src/main.js
+++ src/main.js
@@ -1,58 +1,71 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
 // ignore this line; omitting it causes TS errors
 exports = 0;
-const http = require("http");
 let ASH_HEIGHT = 16.8; // 10cm units
 let ASH_WEIGHT = 430; // 100g units
 let field = document.getElementById("pokemon");
 let button = document.getElementById("submit");
-button.onclick = function () {
+function findPokemon() {
+    try {
+        let result = getPokemonInfo(getInput());
+        displayResults(result);
+    }
+    catch (e) {
+        display(e);
+    }
+}
+function getInput() {
     // retrieves pokemon_name after something has been entered into the field
     let pokemon_name = field.value;
     display("Searching " + pokemon_name + "...");
     if (/^\s*&/.test(pokemon_name)) {
         display("Please enter an input into the text field!");
     }
-    // retrieve Pokemon information from pokeapi.co
+    // prepare query on pokeapi
     let base_string = "http://pokeapi.co/api/v2/pokemon/";
     let query = base_string + pokemon_name.toLowerCase();
+    return query;
+}
+function getPokemonInfo(query) {
     let exists = false;
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', query, false);
+    xhr.send(null);
+    if (xhr.status !== 200) {
+        throw ("Sorry, that Pokemon doesn't seem to exist!");
+    }
+    else {
+        return xhr.responseText;
+    }
+}
+function displayResults(data) {
     let result = {};
-    http.get(query, (res) => {
-        const { statusCode } = res;
-        if (statusCode !== 200) {
-            display("Sorry, that Pokemon doesn't seem to exist!");
-            return;
+    try {
+        let parsedData = JSON.parse(data);
+        result = parsedData;
+        // parse Pokemon data to get height and weight
+        // then decide if Ash is bigger
+        if (result !== {}) {
+            let h = result["height"];
+            let w = result["weight"];
+            let n = result["name"];
+            let isLargerThanAsh = false;
+            isLargerThanAsh = (h > ASH_HEIGHT && w > ASH_WEIGHT);
+            let name = capitalize(n);
+            // display results
+            display(name + " is " + ((isLargerThanAsh) ? "larger" : "not larger") + " than Ash!");
+            return isLargerThanAsh;
         }
-        let data = '';
-        res.on('data', (chunk) => { data += chunk; });
-        res.on('end', () => {
-            try {
-                let parsedData = JSON.parse(data);
-                result = parsedData;
-                // parse Pokemon data to get height and weight
-                // then decide if Ash is bigger
-                if (result !== {}) {
-                    let h = result["height"];
-                    let w = result["weight"];
-                    let n = result["name"];
-                    let isLargerThanAsh = false;
-                    isLargerThanAsh = (h > ASH_HEIGHT && w > ASH_WEIGHT);
-                    console.log(h + " " + w);
-                    let name = n.charAt(0).toUpperCase() + n.slice(1);
-                    display(name + " is " + ((isLargerThanAsh) ? "larger" : "not larger") + " than Ash!");
-                }
-            }
-            catch (e) {
-                console.error(e.message);
-            }
-        });
-        res.on('error', (err) => {
-            display("Whoa");
-        });
-    });
-};
+    }
+    catch (e) {
+        console.error(e.message);
+    }
+    return false;
+}
+function capitalize(n) {
+    return n.charAt(0).toUpperCase() + n.slice(1);
+}
 function display(res) {
     document.getElementById("response").innerHTML = res;
 }
+button.onclick = findPokemon;
--- src/main.ts
+++ src/main.ts
@@ -10,8 +10,20 @@ let ASH_WEIGHT = 430; // 100g units
 let field = document.getElementById("pokemon");
 let button = document.getElementById("submit");
 
-button.onclick = function() {
 
+
+function findPokemon() {
+    try {
+        let result: string = getPokemonInfo(getInput());
+        displayResults(result);
+    } catch (e) {
+        display(e);
+    }
+
+}
+
+
+function getInput() : string {
 
     // retrieves pokemon_name after something has been entered into the field
     let pokemon_name : string = (<HTMLInputElement>field).value;
@@ -21,63 +33,72 @@ button.onclick = function() {
         display("Please enter an input into the text field!");
     }
 
-    // retrieve Pokemon information from pokeapi.co
+    // prepare query on pokeapi
     let base_string = "http://pokeapi.co/api/v2/pokemon/";
     let query = base_string + pokemon_name.toLowerCase();
 
+    return query;
+
+}
+
+function getPokemonInfo(query : string) : string {
+
     let exists : boolean = false;
+
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET',query,false);
+
+    xhr.send(null);
+
+    if (xhr.status !== 200) {
+        throw ("Sorry, that Pokemon doesn't seem to exist!");
+    } else {
+        return xhr.responseText;
+    }
+}
+
+
+function displayResults(data : string) : boolean {
+
     let result : any = {};
-    http.get(query, (res : any) => {
 
-        const { statusCode } = res;
-        if (statusCode !== 200) {
-            display ("Sorry, that Pokemon doesn't seem to exist!");
-            return;
+    try {
+        let parsedData = JSON.parse(data);
+        result = parsedData;
+
+        // parse Pokemon data to get height and weight
+        // then decide if Ash is bigger
+        if (result !== {}) {
+            let h : number = <number> result["height"];
+            let w : number = <number> result["weight"];
+            let n : string = <string> result["name"];
+
+            let isLargerThanAsh = false;
+            isLargerThanAsh = (h > ASH_HEIGHT && w > ASH_WEIGHT);
+
+            let name = capitalize(n);
+
+            // display results
+            display(name + " is " + ((isLargerThanAsh) ?  "larger" : "not larger") + " than Ash!");
+            return isLargerThanAsh;
         }
 
-        let data : string = '';
-        res.on('data', (chunk : string) => { data += chunk; });
+    } catch (e) {
+        console.error(e.message);
+    }
 
-        res.on('end', () => {
-            try {
-                let parsedData = JSON.parse(data);
-                result = parsedData;
+    return false;
+}
 
-                // parse Pokemon data to get height and weight
-                // then decide if Ash is bigger
-                if (result !== {}) {
-                    let h : number = <number> result["height"];
-                    let w : number = <number> result["weight"];
-                    let n : string = <string> result["name"];
-
-                    let isLargerThanAsh = false;
-                    isLargerThanAsh = (h > ASH_HEIGHT && w > ASH_WEIGHT);
-                    console.log(h + " " + w);
-
-                    let name = n.charAt(0).toUpperCase() + n.slice(1);
-
-                    display(name + " is " + ((isLargerThanAsh) ?  "larger" : "not larger") + " than Ash!");
-                }
-
-            } catch (e) {
-                console.error(e.message);
-            }
-        });
-
-        res.on('error', (err : any) => {
-            display("Whoa");
-        })
-
-        }
-
-    );
+function capitalize(n : string) : string {
+    return n.charAt(0).toUpperCase() + n.slice(1);
+}
 
 
-
-
-
-};
 
 function display(res : string) {
     document.getElementById("response").innerHTML = res;
 }
+
+
+button.onclick = findPokemon;
